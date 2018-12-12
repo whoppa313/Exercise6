6dcb214ed75757e312e8e233e05edb5efc508d09
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-12 14:38:04 +0100

    Exercise6

---
+++ src/main/java/Lexicon_Java_victor/Exercise6/App.java
@@ -0,0 +1,25 @@
+package Lexicon_Java_victor.Exercise6;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    
+    	
+    	Scanner in = new Scanner(System.in);
+    	
+    	int nummer1 = in.nextInt();
+    	int nummer2 = in.nextInt();
+    	
+    	System.out.println(nummer1 + nummer2 + " result of addition");
+    	System.out.println(nummer1 - nummer2 + " result of substracktion");
+    	System.out.println(nummer1 * nummer2 + " result of multiplikation");
+    	System.out.println(nummer1 / nummer2 + " result of division");
+    	
+    	
+    	
+    	
+    }
+}
